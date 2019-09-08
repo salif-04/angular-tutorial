c8005f416552d99c04d4ffc4f7e7afe85a5e496c
salif-04 <syedsalifmoin@gmail.com>
2019-09-08 16:49:56 +0530

    Adds 3.1 - Sets up basic component structure

---
+++ 3. Course Project-The Basics/prj-start/src/app/app.module.ts
@@ -0,0 +1,31 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { RecepiesComponent } from './recepies/recepies.component';
+import { RecepieDetailsComponent } from './recepies/recepie-details/recepie-details.component';
+import { RecepieListComponent } from './recepies/recepie-list/recepie-list.component';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    RecepiesComponent,
+    ShoppingListComponent,
+    ShoppingEditComponent,
+    RecepieDetailsComponent,
+    RecepieListComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
