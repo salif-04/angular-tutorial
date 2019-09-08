fd5ad60d1e9e9bd93a6b9b6b5c9dc2f5c2f73e98
salif-04 <syedsalifmoin@gmail.com>
2019-09-08 17:53:35 +0530

    Adds 3.1 - Adds new Recepie Model for recepie details

--- 3. Course Project-The Basics/prj-start/src/app/recepies/recepie-list/recepie-list.component.ts
+++ 3. Course Project-The Basics/prj-start/src/app/recepies/recepie-list/recepie-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recepie } from '../recepie.model';
 
 @Component({
     selector: 'app-recepie-list',
@@ -6,6 +7,10 @@ import { Component, OnInit } from '@angular/core';
     styleUrls: ['recepie-list.component.css']
 })
 export class RecepieListComponent implements OnInit {
+    recepies: Recepie[] = [
+        new Recepie('Burger', 'A tasty li\'l Burger', 'https://www.seriouseats.com/assets_c/2015/07/20150702-sous-vide-hamburger-anova-16-thumb-1500xauto-424812.jpg'),
+        new Recepie('Pizza', 'Yummy Black Olive pizza', 'https://www.seriouseats.com/assets_c/2015/07/20150702-sous-vide-hamburger-anova-16-thumb-1500xauto-424812.jpg')
+    ];
     constructor() { }
 
     ngOnInit(): void { }
